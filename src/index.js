67d242fa58fce7bccd4de9dae600d0d068a530fa
Nedim Filipovic <nedimfilipovic@Nedims-MacBook-Pro.local>
2020-03-29 21:33:28 -0700

    Ejected CRA and built custom webpack for our simpler needs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+// const config = {
+  //   apiKey: process.env.REACT_APP_API_KEY,
+  //   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  //   databaseURL: process.env.REACT_APP_DATABASE_URL,
+  //   projectId: process.env.REACT_APP_PROJECT_ID,
+  //   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  //   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // }
+  // firebase.initializeApp(config);
+  
+  // console.log(firebase);
+  
+  // ReactDOM.render(
+    //   <Provider store={store}>
+    //     <App />
+    //   </Provider>,
+    //   document.getElementById("root")
+    // );
+    
+    // // If you want your app to work offline and load faster, you can change
+    // // unregister() to register() below. Note this comes with some pitfalls.
+    // // Learn more about service workers: https://bit.ly/CRA-PWA
+    // serviceWorker.register();
+    
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './store';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-
+console.log(process.env.REACT_APP_API_KEY);
+console.log(typeof process.env.REACT_APP_API_KEY);
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -19,16 +49,42 @@ const config = {
 }
 firebase.initializeApp(config);
 
-console.log(firebase);
+  
+    
+class Form extends Component {
+  constructor() {
+    super();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+    this.state = {
+      value: ""
+    };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState(() => {
+      return {
+        value
+      };
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+}
+
+export default Form;
+
+const wrapper = document.getElementById("container");
+wrapper ? ReactDOM.render(<Form />, wrapper) : false;
