c1f4125b5cee7b6fc43a3a94852e745a6dfe9eb3
Nedim Filipovic <nedimfilipovic@Nedims-MacBook-Pro.local>
2020-04-07 22:12:16 -0700

    Built game grid generator

--- src/components/routes/user.js
+++ src/components/routes/user.js
@@ -12,6 +12,9 @@ const mapDispatchToProps = (dispatch, props) => {
 }
     
 class User extends Component {
+  componentDidMount() {
+    _generateGameGrid();
+  }
 
   render() {
     return (
@@ -21,3 +24,77 @@ class User extends Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(User);
+
+// helpers
+
+function _generateGameGrid() {
+  const board = {};
+  const maxDistance = 5;
+  const pointsOutOfBound = {
+    '5,0,5': true,
+    '5,1,4': true,
+    '4,-1,5': true,
+    '-5,0,-5': true,
+    '-5,-1,-4': true,
+    '-4,1,-5': true,
+    '0,5,-5': true,
+    '1,5,-4': true,
+    '-1,4,-5': true,
+    '0,-5,5': true,
+    '1,-4,5': true,
+    '-1,-5,4': true,
+    '5,5,0': true,
+    '5,4,1': true,
+    '4,5,-1': true,
+    '-5,-5,0': true,
+    '-4,-5,1': true,
+    '-5,-4,-1': true,
+  };
+  const tileCenters = {
+    '0,0,0': true,
+    '0,-3,3': true,
+    '0,3,-3': true,
+    '1,-1,2': true,
+    '1,2,-1': true,
+    '2,1,1': true,
+    '2,-2,4': true,
+    '2,4,-2': true,
+    '3,0,3': true,
+    '3,3,0': true,
+    '4,2,2': true,
+    '-1,1,-2': true,
+    '-1,-2,1': true,
+    '-2,-1,-1': true,
+    '-2,-4,2': true,
+    '-2,2,-4': true,
+    '-3,0,-3': true,
+    '-3,-3,0': true,
+    '-4,-2,-2': true,
+  };
+
+  for (let i = 0; i <= maxDistance; i++) {
+
+    for (let x = -i; x <= i; x++) {
+      for (let y = -i; y <= i; y++) {
+        const z = x - y;
+
+        // check that distance is correct
+        if (_calculateManhattanDistance({ x: 0, y: 0, z: 0 }, { x, y, z }) === i) {
+          const coordinate = `${x},${y},${z}`;
+          board[coordinate] = pointsOutOfBound[coordinate] ?
+            null
+            :
+            {
+              type: tileCenters[coordinate] ? 'tile' : 'point',
+            }
+          }
+      }
+    }
+  }
+
+  console.log('board', board, Object.keys(board).length);
+}
+
+function _calculateManhattanDistance(origin, point) {
+  return (Math.abs(origin.x - point.x) + Math.abs(origin.y - point.y) + Math.abs(origin.z - point.z)) / 2 
+}
