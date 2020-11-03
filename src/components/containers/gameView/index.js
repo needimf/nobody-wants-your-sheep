189b13bf6f82094e411fd89d7fe3915acf468f5b
irfan-f <irfan.filipovic23@gmail.com>
2020-11-02 17:25:10 -0800

    3D to 2D point system, with meshes

--- src/components/containers/gameView/index.js
+++ src/components/containers/gameView/index.js
@@ -15,12 +15,7 @@ const mapDispatchToProps = (dispatch, props) => {
   return ({})
 }
 
-function Hex({index, tile}) {
-  console.log('HEX tile' + tile);
-  const positions = [[0,0,0], [1.73,0,0], [.868,0,1.49], [-.868,0,1.49], [-1.73, 0,0], [-.868, 0, -1.49],
-                    [.868, 0, -1.49], [2.598, 0, -1.49], [3.46, 0, 0], [2.598, 0, 1.49], [1.73, 0, 2.98],
-                    [0,0,2.98], [-1.73, 0, 2.98], [-2.598, 0, 1.49],[-3.46, 0, 0], [-2.598, 0, -1.49],
-                    [-1.73, 0, -2.98], [0, 0, -2.98], [1.73, 0, -2.98]]
+function Hex({position, tile}) {
   let img;
   switch (tile) {
     case 'ore':
@@ -44,9 +39,9 @@ function Hex({index, tile}) {
   }
   return (
     <>
-      <mesh position={positions[index]} rotation={[0, Math.PI / 2, 0]}>
+      <mesh position={position} rotation={[Math.PI/2, 0, 0]}>
         {/* <arrowHelper></arrowHelper> */}
-        <cylinderBufferGeometry attach="geometry" args={[1,1,.01,6,1,false,11]} />
+        <cylinderBufferGeometry attach="geometry" args={[1,1,.01,6]} />
         <meshBasicMaterial attach="material" map={img} color="white">
         </meshBasicMaterial>
       </mesh>
@@ -93,6 +88,70 @@ function Line({gameBoard}) {
     </>
   )
 }
+
+function Point({gameBoard}) {
+  let board = gameBoard;
+  let tileArray = [];
+  Object.keys(board).map(key => {
+    if(board[key] && board[key].type === 'point') {
+      key.split(',').forEach((string) => {
+        tileArray.push(parseInt(string))
+      });
+    }
+  });
+  let i = 0;
+  while(i < tileArray.length) {
+    let x = tileArray[i];
+    tileArray[i] = tileArray[i+2] * (Math.sqrt(3)/2);
+    tileArray[i+1] = (x + tileArray[i+1]) / 2;
+    tileArray[i+2] = .1;
+    i+=3;
+  }
+
+  var positions = new Float32Array(tileArray);
+
+  return (
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute attachObject={["attributes", "position"]} count={positions.length / 3} array={positions} itemSize={3} />
+      </bufferGeometry>
+      <pointsMaterial attach="material" color="white" size={.2} />
+    </points>
+  )
+}
+
+function BoardRender({gameBoard}) {
+  let board = gameBoard;
+  let tileArray = [];
+  Object.keys(board).map(key => {
+    if(board[key] && board[key].type === 'tile') {
+      key.split(',').forEach((string) => {
+        tileArray.push(parseInt(string))
+      });
+    }
+  });
+  console.log(tileArray);
+  let i = 0;
+  let tileArrayArray = [];
+  while(i < tileArray.length) {
+    let x = tileArray[i];
+    tileArray[i] = tileArray[i+2] * (Math.sqrt(3)/2);
+    tileArray[i+1] = (x + tileArray[i+1]) / 2;
+    tileArray[i+2] = 0;
+    tileArrayArray.push([tileArray[i], tileArray[i+1], tileArray[i+2]]);
+    i+=3;
+  }
+
+  var positions = new Float32Array(tileArray);
+
+  return (
+    tileArrayArray.map((val, index) => {
+      return(
+        <Hex position={val} key={index} tile='hi' />
+      )
+    })
+  )
+}
     
 class GameView extends Component {
   constructor(props) {
@@ -115,123 +174,24 @@ class GameView extends Component {
     }
   }
 
-  makePoints() {
-      let board = this.props.gameState.gameGrid;
-      let tileArray = [];
-      Object.keys(board).map(key => {
-        if(board[key] && board[key].type === 'tile') {
-          key.split(',').forEach((string) => {
-            tileArray.push(parseInt(string))
-          });
-        }
-      });
-      let count = 19;
-      let vertices = []
-      for (let i = 0; i < count; i++) {
-        vertices.push(tileArray[i*3]);
-        vertices.push(tileArray[(i*3)+1]);
-        vertices.push(tileArray[(i*3)+2]);
-        vertices.push(tileArray[i*3]);
-        vertices.push(tileArray[(i*3)+1] + 1);
-        vertices.push(tileArray[(i*3)+2] - 1);
-        vertices.push(tileArray[i*3]+1);
-        vertices.push(tileArray[(i*3)+1]+1);
-        vertices.push(tileArray[(i*3)+2]);
-        vertices.push(tileArray[i*3]+1);
-        vertices.push(tileArray[(i*3)+1]);
-        vertices.push(tileArray[(i*3)+2]+1);
-        vertices.push(tileArray[i*3]);
-        vertices.push(tileArray[(i*3)+1]-1);
-        vertices.push(tileArray[(i*3)+2]+1);
-        vertices.push(tileArray[i*3]-1);
-        vertices.push(tileArray[(i*3)+1]-1);
-        vertices.push(tileArray[(i*3)+2]);
-        vertices.push(tileArray[i*3]-1);
-        vertices.push(tileArray[(i*3)+1]);
-        vertices.push(tileArray[(i*3)+2]-1);
-      }
-      let positions = new Float32Array(vertices);
-      this.setState({...this.state, positions: positions}, () => {console.log('done')})
-}
-  // componentDidMount() {
-  //   this.setState({...this.state, lineGeometry: new THREE.BufferGeometry()})
-  // }
-//   //   scene = new THREE.Scene();
-//   //   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1 ,1000 );
-//   //   camera.position.z = 6;
-//   //   camera.position.x = -6;
-//   //   camera.position.y = 6;
-//   //   camera.lookAt(new THREE.Vector3(0,0,0));
-//   //   renderer = new THREE.WebGLRenderer();
-//   //   renderer.setSize( window.innerWidth, window.innerHeight );
-//   //   document.body.appendChild( renderer.domElement ); 
-//   //   var i = 0;
-//   //   geometry = new THREE.Geometry();
-//   //   geometry2 = new THREE.Geometry();
-//   //   while(i < 19) {
-//   //     console.log(`${tileArray[i*3]}, ${tileArray[(i*3)+1]}, ${tileArray[(i*3)+2]}`);
-//   //     geometry.vertices.push(new THREE.Vector3(tileArray[i*3], tileArray[(i*3)+1], tileArray[(i*3)+2]));
-//   //     geometry2.vertices.push(new THREE.Vector3(tileArray[i*3], tileArray[(i*3)+1] + 1, tileArray[(i*3)+2] - 1))
-//   //     geometry2.vertices.push(new THREE.Vector3(tileArray[i*3]+1, tileArray[(i*3)+1]+1, tileArray[(i*3)+2]))
-//   //     geometry2.vertices.push(new THREE.Vector3(tileArray[i*3]+1, tileArray[(i*3)+1], tileArray[(i*3)+2]+1))
-//   //     geometry2.vertices.push(new THREE.Vector3(tileArray[i*3], tileArray[(i*3)+1]-1, tileArray[(i*3)+2]+1))
-//   //     geometry2.vertices.push(new THREE.Vector3(tileArray[i*3]-1, tileArray[(i*3)+1]-1, tileArray[(i*3)+2]))
-//   //     geometry2.vertices.push(new THREE.Vector3(tileArray[i*3]-1, tileArray[(i*3)+1], tileArray[(i*3)+2]-1))
-//   //     var points = [];
-//   //     points.push(new THREE.Vector3(tileArray[i*3], tileArray[(i*3)+1] + 1, tileArray[(i*3)+2] - 1)); //Top Left
-//   //     points.push(new THREE.Vector3(tileArray[i*3]+1, tileArray[(i*3)+1]+1, tileArray[(i*3)+2])); // Top Right
-//   //     points.push(new THREE.Vector3(tileArray[i*3]+1, tileArray[(i*3)+1], tileArray[(i*3)+2]+1)); // Right
-//   //     points.push(new THREE.Vector3(tileArray[i*3], tileArray[(i*3)+1]-1, tileArray[(i*3)+2]+1)); // Bottom Right
-//   //     points.push(new THREE.Vector3(tileArray[i*3]-1, tileArray[(i*3)+1]-1, tileArray[(i*3)+2])); // Bottom Left
-//   //     points.push(new THREE.Vector3(tileArray[i*3]-1, tileArray[(i*3)+1], tileArray[(i*3)+2]-1)); // Left
-//   //     points.push(new THREE.Vector3(tileArray[i*3], tileArray[(i*3)+1]+1, tileArray[(i*3)+2]-1)); // Top Left /To close hexagon
-//   //     var geometryLine = new THREE.BufferGeometry().setFromPoints( points );
-//   //     var line = new THREE.Line( geometryLine, material );
-//   //     scene.add(line);
-//   //     ++i;
-//   //}
-// }
-
-  componentDidUpdate() {
-    if(!this.state.ready) {
-      this.setState({...this.state, ready: true}, () => {
-        this.makePoints();
-      });
-    }
-  }
-
   render() {
-    console.log(this.props.gameState.tileAssignments)
+    console.log(this.props.gameState)
     return (
       <Canvas
-      camera={{ position: [0, 10, 0], fov: 70}}
+      camera={{ position: [0, 0, 10], fov: 70}}
       style={{width: window.innerWidth, height: window.innerHeight}}
       pixelRatio={window.pixelRatio}
       >
-        <axesHelper args={1} />
-        <Suspense fallback={null}>
+        <axesHelper args={10} />
+        {/* <Suspense fallback={null}>
           {Object.values(this.props.gameState.tileAssignments).map((tile, index) => {
             return (<Hex index={index} tile={tile} key={index} />);
           })}
-          {/* <Hex index={0} tile={""} />
-          <Hex index={1} tile={""} />
-          <Hex index={2} tile={""} />
-          <Hex index={3} tile={""} />
-          <Hex index={4} tile={""} />
-          <Hex index={5} tile={""} />
-          <Hex index={6} tile={""} />
-          <Hex index={7} tile={""} />
-          <Hex index={8} tile={""} />
-          <Hex index={9} tile={""} />
-          <Hex index={10} tile={""} />
-          <Hex index={11} tile={""} />
-          <Hex index={18} tile={""} />
-          <Hex index={12} tile={""} />
-          <Hex index={13} tile={""} />
-          <Hex index={14} tile={""} />
-          <Hex index={15} tile={""} />
-          <Hex index={16} tile={""} />
-          <Hex index={17} tile={""} /> */}
+        </Suspense> */}
+        {/* <Point postion={[1, 1, 1]} /> */}
+        <Suspense fallback={null}>
+          <BoardRender gameBoard={this.props.gameState.gameGrid} />
+          <Point gameBoard={this.props.gameState.gameGrid} />
         </Suspense>
         {/* <Line gameBoard={this.props.gameState.gameGrid} /> */}
       </Canvas>
