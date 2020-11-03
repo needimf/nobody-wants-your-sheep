e13a4e09bb73dce540eb6c9cb7cc3ff6496f1b87
Nedim Filipovic <nedim.filipovic@gmail.com>
2020-11-02 23:00:44 -0800

    Added additional initital game state to store, beginning to handle game actions

--- src/store/gamePlay/index.js
+++ src/store/gamePlay/index.js
@@ -4,19 +4,153 @@ const initialState = {
   numberTokenAssignments: {},
 };
 
+const initialPlayerState = {
+  available: {
+    city: 4,
+    settlement: 5,
+    road: 15,
+  },
+  resources: {
+    wood: 0,
+    brick: 0,
+    ore: 0,
+    wheat: 0,
+    sheep: 0,
+  },
+  built: {
+    city: 0,
+    settlement: 0,
+    road: 0,
+  },
+  devCards: {
+    knight: 0,
+    usedKnights: 0,
+    victoryPoint: 0,
+    roadBuilder: 0,
+    monopoly: 0,
+    yearOfPlenty: 0,
+  },
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'initializeGame': {
       const gameGrid = generateGameGrid();
       const { tileAssignments, numberTokenAssignments } = assignTilesAndNumberTokensToNewBoard(gameGrid);
+      const robberLocation = Object.keys(tileAssignments).find((key) => tileAssignments[key] === 'desert');
+      const resourceCards = getResourceCards(action.data.gameType);
+      const developmentCards = getDevCards(action.data.gameType);
 
       return {
         ...state,
         gameGrid,
         tileAssignments,
         numberTokenAssignments,
+        gameType: action.data.gameType,
+        robberLocation,
+        buildings: {},
+        roads: {},
+        resourceCards,
+        developmentCards,
+        players: {
+          1: initialPlayerState,
+          2: initialPlayerState,
+          3: initialPlayerState,
+          4: initialPlayerState,
+        },
       };
     }
+    case 'buildSettlement': {
+      const stateDiff = { ...state };
+
+      stateDiff.buildings = { ...state.buildings };
+      stateDiff.buildings[action.data.coordinate] = {
+        type: 'settlement',
+        player: action.data.player,
+      };
+
+      stateDiff.players = { ...state.players };
+      stateDiff.players[action.data.player] = { ...state.players[action.data.player] };
+      stateDiff.players[action.data.player].available = { ...state.players[action.data.player].available };
+      stateDiff.players[action.data.player].available.settlement -= 1;
+
+      stateDiff.players[action.data.player].built = { ...state.players[action.data.player].built };
+      stateDiff.players[action.data.player].built.settlement += 1;
+
+      return { ...state, ...stateDiff };
+    }
+    case 'buildCity': {
+      const stateDiff = { ...state };
+
+      stateDiff.buildings = { ...state.buildings };
+      stateDiff.buildings[action.data.coordinate] = {
+        type: 'city',
+        player: action.data.player,
+      };
+
+      stateDiff.players = { ...state.players };
+      stateDiff.players[action.data.player] = { ...state.players[action.data.player] };
+      stateDiff.players[action.data.player].available = { ...state.players[action.data.player].available };
+      stateDiff.players[action.data.player].available.settlement += 1;
+      stateDiff.players[action.data.player].available.city -= 1;
+
+      stateDiff.players[action.data.player].built = { ...state.players[action.data.player].built };
+      stateDiff.players[action.data.player].built.settlement -= 1;
+      stateDiff.players[action.data.player].built.city += 1;
+
+      return { ...state, ...stateDiff };
+    }
+    case 'moveRobber': {
+      const stateDiff = { ...state };
+
+      stateDiff.robberLocation = action.data.robberLocation;
+
+      return { ...state, ...stateDiff };
+    }
+    // case 'payResourcesAfterRoll': {
+    //   const stateDiff = { ...state };
+
+    //   let tilesToPayOut = state.numberTokenAssignments[action.data.numberRolled];
+    //   tilesToPayOut = tilesToPayOut.filter((coordinate) => coordinate !== state.robberLocation);
+    //   const coordinatesToPayOut = tilesToPayOut.reduce((acc, tile) => {
+    //     const coordinatesForTile = _getCoordinatesOneDistanceAway(tile);
+    //     acc.push(...coordinatesForTile.map((coordinate) => ({ coordinate, tile })));
+    //     return acc;
+    //   }, []);
+
+    //   coordinatesToPayOut.forEach((coordinate) => {
+    //     const hasBuilding = state.buildings[coordinate.coordinate];
+    //     if (!hasBuilding) return;
+    //     const resource = state.tileAssignments[coordinate.tile];
+    //     const player = hasBuilding.player;
+    //     const count = hasBuilding.type === 'city' ? 2 : 1;
+    //   });
+
+    //   stateDiff.robberLocation = action.data.robberLocation;
+
+    //   return { ...state, ...stateDiff };
+    // }
+    // case 'buildRoad': {
+    //   const stateDiff = { ...state };
+
+    //   stateDiff.buildings = { ...state.buildings };
+    //   stateDiff.buildings[action.data.coordinate] = {
+    //     type: 'city',
+    //     player: action.data.player,
+    //   };
+
+    //   stateDiff.players = { ...state.players };
+    //   stateDiff.players[action.data.player] = { ...state.players[action.data.player] };
+    //   stateDiff.players[action.data.player].available = { ...state.players[action.data.player].available };
+    //   stateDiff.players[action.data.player].available.settlement += 1;
+    //   stateDiff.players[action.data.player].available.city -= 1;
+
+    //   stateDiff.players[action.data.player].built = { ...state.players[action.data.player].built };
+    //   stateDiff.players[action.data.player].built.settlement -= 1;
+    //   stateDiff.players[action.data.player].built.city += 1;
+
+    //   return { ...state, ...stateDiff };
+    // }
     default:
       return state;
   }
@@ -143,8 +277,47 @@ function assignTilesAndNumberTokensToNewBoard(gameBoard) {
   return { tileAssignments, numberTokenAssignments };
 }
 
+function _getCoordinatesOneDistanceAway(coordinate) {
+  const coordinates = [];
+  const [x, y, z] = coordinate.split(',');
+  [-1, 1].forEach((adjustment) => {
+    coordinates.push(`${x},${y + adjustment},${z - adjustment}`);
+    coordinates.push(`${x + adjustment},${y},${z + adjustment}`);
+    coordinates.push(`${x + adjustment},${y + adjustment},${z}`);
+  });
+  return coordinates;
+}
+
 function _calculateManhattanDistance(origin, point) {
   return (Math.abs(origin.x - point.x) + Math.abs(origin.y - point.y) + Math.abs(origin.z - point.z)) / 2 
+}
+
+function getResourceCards(gameType) {
+  switch (gameType) {
+    case 'classic':
+    default:
+      return {
+        wood: 19,
+        brick: 19,
+        ore: 19,
+        sheep: 19,
+        wheat: 19,
+      };
+  }
+}
+
+function getDevCards(gameType) {
+  switch (gameType) {
+    case 'classic':
+    default:
+      return {
+        knight: 14,
+        victoryPoint: 5,
+        roadBuilder: 2,
+        monopoly: 2,
+        yearOfPlenty: 2,
+      };
+  }
 }
 
 const TILES = {
