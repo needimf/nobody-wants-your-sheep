b39f4095e024f5526ca3fe75481ccec4d47e892c
Nedim Filipovic <nedim.filipovic@gmail.com>
2020-11-27 18:50:43 -0800

    updated babel to work with async/await

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react'],
+          },
         },
       },
       {
